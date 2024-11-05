4dc62545221b7af4162a6a1edf4a4b4901738d54
Rodrigo Maidana <114775631+romaidana29@users.noreply.github.com>
2024-11-05 00:54:53 -0300

    Primer commit

---
+++ src/main/java/com/fiuni/mytube_gateway/MytubeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fiuni.mytube_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytubeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytubeGatewayApplication.class, args);
+    }
+
+}
