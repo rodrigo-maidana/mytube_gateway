22134f270d4902bfc6926fea6c1d924b9188dae7
Rodrigo Maidana <ro-maidana@hotmail.com>
2024-11-06 21:05:12 -0300

    working 2

--- src/main/java/com/fiuni/mytube_gateway/config/SecurityConfig.java
+++ src/main/java/com/fiuni/mytube_gateway/config/SecurityConfig.java
@@ -2,25 +2,22 @@ package com.fiuni.mytube_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Configuración de seguridad
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll()
                 )
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable);
-
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // Deshabilitar CSRF
+                .cors(ServerHttpSecurity.CorsSpec::disable); // Deshabilitar CORS
         return http.build();
     }
 }
